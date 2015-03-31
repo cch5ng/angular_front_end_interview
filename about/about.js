53447c41632463d4afbbf99e62300eff5b8f5543
cch5ng <cch5ng@gmail.com>
2015-03-31 11:29:51 -0700

    updated about content. updated route and template info for about module

--- about/about.js
+++ about/about.js
@@ -1,14 +1,14 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.about', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
+  $routeProvider.when('/about', {
+    templateUrl: 'about/about.html',
+    controller: 'AboutCtrl'
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('AboutCtrl', [function() {
 
 }]);
