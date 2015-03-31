781d4d5469d83dbaed32823dfd8151a51ac450fc
cch5ng <cch5ng@gmail.com>
2015-03-31 11:28:45 -0700

    replaced linkes /ref for view2 to about

--- app/app.js
+++ app/app.js
@@ -3,9 +3,9 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
   'myApp.main',
+//  'myApp.view1',
+  'myApp.about',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
