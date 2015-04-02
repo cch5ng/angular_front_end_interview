d2918a51a81cf09a38aaf7dfeb98a7913915bce2
cch5ng <cch5ng@gmail.com>
2015-04-02 10:47:15 -0700

    updated to point to current modules. added placeholders for modules with undefined tests

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,9 @@ module.exports = function(config){
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/components/**/*.js',
-      'app/view*/**/*.js'
+      'app/main/**/*.js' //,
+      //'app/all/**/*.js',
+      //'app/about/**/*.js'
     ],
 
     autoWatch : true,
