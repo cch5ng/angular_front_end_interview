e211ab51418f2e3a6de88198c4b84487ba63714e
cch5ng <cch5ng@gmail.com>
2015-04-03 14:18:21 -0700

    added injection for $rootScope to fix controller test. tried to fix service function test but it still fails

--- app/main/main_test.js
+++ app/main/main_test.js
@@ -2,33 +2,39 @@
 
 describe('myApp.main module', function() {
 
-  beforeEach(module('myApp.main'));
+	//var ngSanitize;
+	var scope;
+
+  beforeEach(function() {
+  	module('myApp.main');
+  });
 
   describe('main controller', function(){
 
-    it('should ....', inject(function($controller) {
+    it('should ....', inject(function($controller, $rootScope) {
       //spec body
-      var myInterviewFormCtrl = $controller('interviewFormCtrl');
+      scope = $rootScope.$new();
+      var myInterviewFormCtrl = $controller('interviewFormCtrl', {$scope: scope});
       expect(myInterviewFormCtrl).toBeDefined();
     }));
 
   });
 
+});
 ////
 
-	var dataCollection;
-
-	beforeEach(inject(function(_dataCollection_) {
-		dataCollection = _dataCollection_;
-	}));
-
 	describe('fixQuestionsObj method', function() {
-			var updatedCategoriesAr;
+			var DataCollectionService;
 
-		beforeEach(function() {
-			var categoriesAr = [{category: 'General Questions'}, {category: 'HTML Questions'}, {category: 'CSS Questions'}, {category: 'Javascript Questions'}, {category: 'Network Questions'}, {category: 'Coding Questions'}, {category: 'Fun Questions'}];
-			updatedCategoriesAr = dataCollection.fixQuestionsObj(categoriesAr);
+	beforeEach(function() {
+  	module('myApp.main');
+		inject(function($injector) { //verify this syntax for cur version of angular
+			DataCollectionService = $injector.get('dataCollection');
 		});
+	});
+
+		var categoriesAr = [{category: 'General Questions'}, {category: 'HTML Questions'}, {category: 'CSS Questions'}, {category: 'Javascript Questions'}, {category: 'Network Questions'}, {category: 'Coding Questions'}, {category: 'Fun Questions'}];
+		var updatedCategoriesAr = DataCollectionService.fixQuestionsObj(categoriesAr); //keeps failing
 
 		it('should swap the categories, Fun Questions and Coding Questions', function() {
 			expect(updatedCategoriesAr[5].category.toBe('Fun Questions'));
@@ -37,4 +43,3 @@ describe('myApp.main module', function() {
 
 	});
 
-});
